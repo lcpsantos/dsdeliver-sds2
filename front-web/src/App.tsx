89372a1238e379adffd8150540cc6cded1fe582d
Luiz César <lcpsantos@gmail.com>
2021-01-10 22:01:24 -0300

    Add page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navibar';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
