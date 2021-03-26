36a00a86ea4ccfb945a22d48c3382735d381c7b3
Luiz César <lcpsantos@gmail.com>
2021-03-26 01:08:44 -0300

    branch para corrigir o problema do mapa

--- .history/front-web/src/api_20210325234248.ts
+++ .history/front-web/src/api_20210325234248.ts
@@ -1,16 +1,13 @@
 import axios from "axios";
 import { OrderPayLoad } from "./Orders/types";
 
-//const API_URL = process.env.REACT_APP_API_URL;
-const API_URL = 'http://localhost:8080'
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken=process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts(){
     return axios(`${API_URL}/products`)
 }
 
-
-// abrir o mapbox para ativar o token
 export function fetchLocalMapBox(local:string){
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
 }
